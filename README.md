446925cae5176c2c898d813f4c59381a7fe0d224
David Jiménez <dubisdev@gmail.com>
2023-12-10 21:14:46 +0100

    fix: simplify files, focus on windows support

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 With this lib you can retrieve the system icon which is associated
 to a certain file extension. The icon will be in the .png format.
-Windows, macOS and Linux (GTK) are supported.
+Only Windows is supported.
--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,9 @@
 //!
 //! With this lib you can retrieve the system icon which is associated
 //! to a certain file extension. The icon will be in the .png format.
-//! Windows and Linux (GTK) are supported.
+//! Windows is supported
 //!
 //! When you specify an absolute path to a .exe file, then the icon is loaded from resource, if the exe contains an icon resource.
-#[cfg(target_os = "windows")]
 use image::ImageError;
 use std::{fmt, str::Utf8Error};
 
@@ -13,7 +12,6 @@ use std::{fmt, str::Utf8Error};
 pub enum InnerError {
     IoError(std::io::Error),
     Utf8Error(Utf8Error),
-    #[cfg(target_os = "windows")]
     ImageError(ImageError),
 }
 
@@ -41,7 +39,6 @@ impl From<Utf8Error> for Error {
     }
 }
 
-#[cfg(target_os = "windows")]
 impl From<ImageError> for Error {
     fn from(error: ImageError) -> Self {
         Error {
@@ -62,35 +59,17 @@ impl fmt::Debug for InnerError {
         let res = match self {
             &InnerError::Utf8Error(_) => "Utf8Error".to_string(),
             &InnerError::IoError(_) => "IoError".to_string(),
-            #[cfg(target_os = "windows")]
             &InnerError::ImageError(_) => "ImageError".to_string(),
         };
         write!(f, "(Error type: {}", res)
     }
 }
 
-#[cfg(not(any(target_os = "macos", target_os = "windows")))]
-mod linux;
-#[cfg(not(any(target_os = "macos", target_os = "windows")))]
-use self::linux as imp;
-
-#[cfg(target_os = "macos")]
-mod macos;
-#[cfg(target_os = "macos")]
-use self::macos as imp;
-
-#[cfg(target_os = "windows")]
 mod windows;
-#[cfg(target_os = "windows")]
 use self::windows as imp;
-
-pub enum Icon {
-    Png(Vec<u8>),
-    Svg(Vec<u8>),
-}
 
 /// Retrieving system icon. You have to specify the file path and desired icon size (like 16, 32 or 64).
 /// Returns the icon formatted as png as byte buffer.
-pub fn get_icon(path: &str, size: u16) -> Result<Icon, Error> {
+pub fn get_icon(path: &str, size: u16) -> Result<Vec<u8>, Error> {
     imp::get_icon(path, size)
 }
